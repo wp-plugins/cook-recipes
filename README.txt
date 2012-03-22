1a610dafb0e93ad4c0c79e4d70541c732bede62a
machinecode <machinecode@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-03-22 22:01:16 +0000

    Adding first version of my plugin
    git-svn-id: https://plugins.svn.wordpress.org/cook-recipes/trunk@522389 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ README.txt
@@ -0,0 +1,31 @@
+=== Cook Recipes ===
+Contributors: 
+Donate link: 
+Tags: cook, recipes
+Requires at least: 3.0
+Tested up to: 3.3.1
+Stable tag: 3.0
+
+Custom post type for cook recipes
+
+== Description ==
+
+You can post cook recipes easily with this post but i will create new views for this plugin in next version.
+
+== Usage ==
+
+When you activate the plugin, you will see add new recipe link at admin panel.
+
+== Installation ==
+
+1. Extract the zip file and drop the contents in the wp-content/plugins/ directory of your WordPress installation
+2. Activate the Plugin from Plugins page
+
+== Change log ==
+= 1.0 =
+ * Initial Release
+
+== Frequently Asked Questions ==
+
+= Please send me email! =
+You can contact with me from faruk [at] iyte [.] net then i will create a faq section with your questions.
---
+++ cook-recipes.php
@@ -0,0 +1,111 @@
+<?php
+/*
+Plugin Name: Food Recipes
+Plugin URI: http://blog.omerfarukak.com/yemek-tarifleri
+Description: Custom post type for food recipes.
+Version: 1.0
+Author: Ömer Faruk AK
+Author URI: http://omerfarukak.com
+*/
+
+class AK_Cook_Recipes
+{
+	public function __construct()
+	{
+		$this->register_post_type();
+		$this->taxonomies();
+		$this->metaboxes();
+		register_taxonomy_for_object_type('category', 'ak_cook');
+	}
+
+	public function register_post_type()
+	{
+		$args = array(
+			'public' => true,
+			'label' => 'Cook Recipes',
+			'labels' => array('add_new_item' => 'Add New Recipe',
+					'name' => 'Cook Recipes',
+					'singular_name' => 'Cook Recipe',
+					'add_new' => 'Add New Recipe',
+					'menu_name' => 'Cook Recipes'
+					),
+			'rewrite' => array( 'slug' => 'recipes' ),
+			'supports' => array('title', 'editor', 'thumbnail', 'comments'),
+			'description' => 'You can use this type for cook recipes.',
+			'menu_position' => 5,
+			'menu_icon' => plugin_dir_path(__FILE__); . '/images/food.png'
+		);
+		
+		register_post_type('ak_cook', $args);
+	}
+
+	public function taxonomies()
+	{
+		$taxonomies = array();
+		
+		$taxonomies['tur'] = array(
+		'hierarchical' => true,
+		'query_var' => 'cook_cat',
+		'rewrite' => array(
+				'slug' => 'cook/cat'
+		),
+		'labels' => array(
+				'name' => 'Cook Categories',
+				'singular_name' => 'Cook Category',
+				'edit_item' => 'Edit Category',
+				'update_item' => 'Update Category',
+				'add_new_item' => 'Add Cook Category',
+				'new_item_name' => 'Add New Cook Category',
+				'all_items' => 'All Categories',
+				'search_items' => 'Search in Cooks',
+				'populer_items' => 'Popular Cooks',
+				'add_or_remove_items' => 'Add or remove'
+			)
+		);
+
+		$this->register_all_taxonomies($taxonomies);
+	}
+
+	public function register_all_taxonomies($taxonomies)
+	{
+		foreach($taxonomies as $name => $arr ){
+			register_taxonomy($name, array('ak_cook'), $arr);
+		}
+	}
+
+	public function metaboxes()
+	{
+		add_action('add_meta_boxes', function(){
+			//css id, title, cb func, page, priority, cb func args
+			add_meta_box('ak_cook_malzemeler', 'Malzemeler', 'yemek_malzemeleri', 'ak_cook');
+		});
+
+		function yemek_malzemeleri($post)
+		{
+			$malzemeler = get_post_meta($post->ID, 'ak_cook_malzemeler', true);
+		?>
+			<p>
+				<label for="ak_cook_malzemeler"> Malzemeler : </label>
+				<textarea name="ak_cook_malzemeler" id="ak_cook_malzemeler"  rows="15" cols="100" class="widefat"><?php echo esc_attr($malzemeler); ?></textarea>
+			</p>
+		<?php
+		}
+
+		add_action('save_post', function($id){
+			if ( isset($_POST['ak_cook_ingredients']) )
+			{
+				update_post_meta(
+					$id,
+					'ak_cook_ingredients',
+					strip_tags($_POST['ak_cook_ingredients'])
+				);
+			}
+		});
+	}
+}
+
+add_action('init', function(){
+		new AK_Cook_Recipes();
+});
+
+?>
